6ef5f9256b0e3fe032f0a7f27ef5c5bf15de59a5
Yao Lixin <yaolixinylx@gmail.com>
2014-02-22 14:04:15 +0800

    waste in out judgement changed

--- android/android_end/RC663_RFID_THREE/gen/com/geomobile/rc6633/R.java
+++ android/android_end/RC663_RFID_THREE/gen/com/geomobile/rc6633/R.java
@@ -11,10 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int globe=0x7f020000;
-        public static final int header_logo=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int sjtu=0x7f020003;
+        public static final int bg_border=0x7f020000;
+        public static final int globe=0x7f020001;
+        public static final int header_logo=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int sjtu=0x7f020004;
     }
     public static final class id {
         public static final int LinearLayout01=0x7f050002;
