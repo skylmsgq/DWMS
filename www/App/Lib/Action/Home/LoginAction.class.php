404aa8ce3da7899f261cd437ce029a2785e695b8
Yao Lixin <yaolixinylx@gmail.com>
2014-02-24 17:07:11 +0800

    add modify password feature

--- www/App/Lib/Action/Home/LoginAction.class.php
+++ www/App/Lib/Action/Home/LoginAction.class.php
@@ -23,6 +23,32 @@ class LoginAction extends CommonAction{
 		session_destroy();
 		$this->redirect( 'Home/Index/index' );
 	}
+	public function changepwd() {
+		$old_pass=I('post.old_pass');
+		$new_pass=I('post.new_pass');
+		$userid=I('post.user_id');	
+		$user=M('user')	;
+		$old_pass_real=$user->where("user_id=$userid")->getField('password');
+		if ($old_pass!=$old_pass_real)
+			$this->ajaxReturn( 0 );	//wrong password
+		else
+		{
+			$result=$user->where("user_id=$userid")->setField('password', $new_pass);
+			if ($result)
+				{
+				$modifytime=date( 'Y-m-d H:i:s', time() );
+				$user->where("user_id=$userid")->setField('modify_time', $modifytime);
+				$this->ajaxReturn( 1 );// right
+				}
+			else	
+				$this->ajaxReturn( 2 );// unable to change password
+		}	
+	}
+	public function show_page()
+	{
+		$tmp_content=$this->fetch( './App/Tpl/Home/Common/changepwd.html' );
+		$this->ajaxReturn( $tmp_content );
+	}
 }
 
 ?>
