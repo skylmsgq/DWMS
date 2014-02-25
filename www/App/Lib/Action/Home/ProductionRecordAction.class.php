be4e6ff27d62d44498ce0e843e07e4bf9bc79dd3
skylmsgq <skylmsgq@gmail.com>
2014-02-25 22:15:10 +0800

    checkbox

--- www/App/Lib/Action/Home/ProductionRecordAction.class.php
+++ www/App/Lib/Action/Home/ProductionRecordAction.class.php
@@ -19,19 +19,23 @@ class ProductionRecordAction extends CommonAction{
 		$reception_unit_json = json_encode($reception_unit);
 		$transport_unit_json = json_encode($transport_unit);
 		$this->unit = $unit;
+
+		$rfid = M( 'rfid' )->where(array( 'rfid_status' => 3, 'transfer_status' => 0,'production_unit_id'=>session('production_unit_id')))->select();
+		$rfid_json = json_encode($rfid);
+
 		$tmp_content=$this->fetch( './Public/html/Content/Production/record/transfer_record_request.html' );
-		$tmp_content="<script>reception_unit=$reception_unit_json;transport_unit=$transport_unit_json;</script> $tmp_content";
+		$tmp_content="<script>rfid = $rfid_json;reception_unit=$reception_unit_json;transport_unit=$transport_unit_json;</script> $tmp_content";
 		$this->ajaxReturn( $tmp_content );
 	}
 
 	// 转移备案->转移备案申请->rfid 选择
-	public function rfid_select(){
-		$rfid = M( 'rfid' )->where(array( 'rfid_status' => 3, 'production_unit_id'=>session('production_unit_id')))->select();
-		$rfid_json = json_encode($rfid);
-		$tmp_content=$this->fetch( './Public/html/Content/Production/record/rfid_select.html' );
-		$tmp_content="<script>rfid = $rfid_json;</script> $tmp_content";
-		$this->ajaxReturn( $tmp_content );
-	}
+	// public function rfid_select(){
+	// 	$rfid = M( 'rfid' )->where(array( 'rfid_status' => 3, 'production_unit_id'=>session('production_unit_id')))->select();
+	// 	$rfid_json = json_encode($rfid);
+	// 	$tmp_content=$this->fetch( './Public/html/Content/Production/record/rfid_select.html' );
+	// 	$tmp_content="<script>rfid = $rfid_json;</script> $tmp_content";
+	// 	$this->ajaxReturn( $tmp_content );
+	// }
 
 	// 转移备案->转移备案申请->表单提交
 	public function transfer_record_request_form() {
@@ -40,6 +44,10 @@ class ProductionRecordAction extends CommonAction{
 		$time = date( 'Y-m-d H:i:s', time() );
 		$record->record_add_time = $time;
 		$record->record_modify_time = $time;
+
+		$record->rfid_table_id = I( 'post.rfid_table_id' );
+		$record->predict_output_quantity = I( 'post.predict_output_quantity' );
+		$record->predict_output_weight = I( 'post.predict_output_weight' );
 
 		$record->production_unit_id = session( 'production_unit_id' );
 		$record->record_code = session( 'production_unit_id' ) . '-' . date( 'Y-m' ) . '-' . ( M( 'record' )->max( 'record_id' )+1 );
