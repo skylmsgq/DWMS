9faf85fed0ebcf80d8598c4ded49f52c598d2a73
zhaohui <zhangzhaohui@sjtu.edu.cn>
2014-02-25 23:02:16 +0800

    modify gps socketserver

--- gps/socketserver/DB.php
+++ gps/socketserver/DB.php
@@ -1,21 +1,11 @@
 <?php
-/**
- * SelectSocketServer Class
- * By James.Napu(zhangzhaohui@sjtu.edu.cn)
-**/
-set_time_limit(0);
-date_default_timezone_set("PRC");
-
-class SelectSocketServer 
+class DatabaseDo 
 {
-	private static $socket;
-	private static $timeout = 60;
-	private static $maxconns = 1024;
-	private static $connections = array();
+
 	function writeLog($msg)
 	{
 		$logFile = date('Y-m-d').'.txt';
-		$message = date('Y-m-d H:i:s').' >>> '.$msg."\r\n";
+		$message = date('Y-m-d H:i:s').' >>> '.$msg."\n";
 		file_put_contents($logFile,$message,FILE_APPEND );
 	}
 	
@@ -60,7 +50,7 @@ class SelectSocketServer
 			return 0.0;
 		}
 		$shortD = $this->dist2($p,$arr[0]);
-		for ($i = 0;$i<count($arr);$i++)
+		for ($i = 0;$i<count($arr)-1;$i++)
 		{
 			$cur = $this->distToSegment($p,$arr[$i],$arr[$i+1]);
 			if($cur<$shortD)
@@ -70,6 +60,7 @@ class SelectSocketServer
 		}
 		return $shortD;
 	}
+	
 	function shortDist($p_json,$arr_json)
 	{
 		$p = $this->pointFromLatLng($p_json->lng,$p_json->lat);
@@ -93,8 +84,8 @@ class SelectSocketServer
 
 	    return($num/60+$i);
 	} 
-	function getMap($long,$lat){
-
+	function getMap($long,$lat)
+	{
 		$url = 'http://api.map.baidu.com/ag/coord/convert';
 		$method = 'get';
 		//echo $long.'  '.$lat;
@@ -109,7 +100,7 @@ class SelectSocketServer
 			$blong = base64_decode($json_data->x);
 			$blat = base64_decode($json_data->y);
 		}else{
-			$msg = "访问百度API错误";
+			$msg = "Cann't access Baidu API";
 			$this->writeLog($msg);
 		}
 			//echo $blong.','.$blat;
@@ -117,13 +108,12 @@ class SelectSocketServer
 	}
 
 
-	function httpRequest($url,$method,$params=array()){
+	function httpRequest($url,$method,$params=array())
+	{
 
 			if(trim($url)==''||!in_array($method,array('get','post'))||!is_array($params)){
-				//echo 'false';
 				return false;
 			}
-			//echo 'right';
 			$curl=curl_init();
 			curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
 			curl_setopt($curl,CURLOPT_HEADER,0 ) ;
@@ -135,7 +125,6 @@ class SelectSocketServer
 					}
 					$str=substr($str,0,-1);
 					$url.=$str;
-					//echo $url;
 					curl_setopt($curl,CURLOPT_URL,$url);
 				break;
 				case 'post':
@@ -152,18 +141,19 @@ class SelectSocketServer
 				$res=curl_exec($curl);
 			}
 			curl_close($curl);
-			//echo $result;
 			return $res;	
 	} 
 	
 	
-	function InsertDB($line){
+	public function InsertDB($line)
+	{
 
-		if(strpos($line,"#")>3){
+		if(strpos($line,"#")>3)
+		{
 			$con = mysql_connect('10.50.6.70', 'root', 'root1234');
 			if (!$con)
 			{
-				$msg = "数据库连接失败".mysql_error();
+				$msg = "Could not connect:".mysql_error();
 				$this->writeLog($msg);
 				die('Could not connect: ' . mysql_error());
 			}
@@ -174,20 +164,20 @@ class SelectSocketServer
 			$speed = 0.0;
 			$heigh = 0.0;
 			$time = date("Y-m-d H:i:s");
-			$id = substr($line,0,strpos($line,"#"));
+			$id = trim(substr($line,0,strpos($line,"#")));
 			$blong = 0.0;
 			$blat = 0.0;
+			$wanderR = 0.0;
 			if((bool)strpos($line,"A")){
-				$latitude =  substr($line,strpos($line,"N")+2,strpos($line,"E")-strpos($line,"N")-3);
-				$longitude = substr($line,strpos($line,"E")+2,strpos($line,"V")-strpos($line,"E")-3);
-				$speed = substr($line,strpos($line,"V")+2,strpos($line,"H")-strpos($line,"V")-3);
-				$heigh = substr($line,strpos($line,"H")+2,strlen($line)-strpos($line,"H")-5);
+				$latitude =  trim(substr($line,strpos($line,"N")+2,strpos($line,"E")-strpos($line,"N")-3));
+				$longitude = trim(substr($line,strpos($line,"E")+2,strpos($line,"V")-strpos($line,"E")-3));
+				$speed = trim(substr($line,strpos($line,"V")+2,strpos($line,"H")-strpos($line,"V")-3));
+				$heigh = trim(substr($line,strpos($line,"H")+2,strlen($line)-strpos($line,"H")-5));
 				$status = 0;
 				$rest = $this->getMap($longitude,$latitude);
 				list($blong,$blat)= split(',',$rest);
-				$p_json->x = $blong;
-				$p_json->y = $blat;
-				$wanderR = 0.0;
+				$p_json->lng = $blong;
+				$p_json->lat = $blat;
 				$result = mysql_query("SELECT device_id FROM device WHERE device_serial_num='".$id."'");
 				if(mysql_num_rows($result)){
 					while($row = mysql_fetch_array($result))
@@ -236,150 +226,64 @@ class SelectSocketServer
 												$alarmDistance = $row['alarm_distance'];
 											}
 											if($wanderR>=$alarmDistance){
-												$sql2 = "INSERT INTO alarm (alarm_date_time, alarm_longitude, alarm_latitude, vehicle_offset_distance, vehicle_id, alarm_add_time, alarm_status, agency_id) VALUES ('$time', '$long', '$lat', '$wanderR', '$vehicleId', '$time', '0', '$agencyId')";
+												$sql2 = "INSERT INTO alarm (alarm_date_time, alarm_longitude, alarm_latitude, vehicle_offset_distance, vehicle_id, alarm_add_time, alarm_status, agency_id) VALUES ('$time', '$blong', '$blat', '$wanderR', '$vehicleId', '$time', '0', '$agencyId')";
 												if(!mysql_query($sql2,$con)){	
-													$msg =  "插入告警数据失败：".mysql_error();
+													$msg =  "Could not insertDB：".mysql_error();
 													$this->writeLog($msg);
 												}
 											}
 										}else{
-											$msg = "权限id为:".$jurisdictionId."没有规划告警距离";
+											$msg = "jurisdiction id:".$jurisdictionId."does not design alarmdistance";
 											$this->writeLog($msg);
 										}
 										
 									
 									}else{
-										$msg = "权限id为:".$jurisdictionId."没有绑定环保局";
+										$msg = "jurisdiction id:".$jurisdictionId."does not bind distrct";
 										$this->writeLog($msg);
 									}
 									
 								
 								}else{
-									$msg = "运输企业id为:".$transportId."没有直属环保局";
+									$msg = "transport id:".$transportId."does not bind distrct";
 									$this->writeLog($msg);
 								
 								}
 								
 							}else{
-								$msg = "路线id为:".$routeId."没有规划路线";
+								$msg = "route id:".$routeId."does not design route";
 								$this->writeLog($msg);
 							}
 							
 						}else{
-							$msg = "车辆id为:".$vehicleId."没有绑定路线";
+							$msg = "vehicle id:".$vehicleId."does not bind route";
 							$this->writeLog($msg);
 						}
 						
 					}else{
-						$msg = "设备id为:".$deviceId."没有绑定车辆";
+						$msg = "device id:".$deviceId."does not bind vehicle";
 						$this->writeLog($msg);
 					}
 					
 				}else{
-					$msg = "没有找到序列号为:".$id."的设备";
+					$msg = "could not find device id :".$id;
 					$this->writeLog($msg);
 				}
 						
 			}
 			$tableName = trim("gps_".$id);
-			$sql = "INSERT INTO $tableName (datetime,longitude,latitude,bmap_longitude,bmap_latitude,height,speed,status,offset_distance, vehicle_id) VALUES ('$time','$longitude','$latitude','$blong','$blat','$heigh','$speed','$status','$wanderR', '$vehicleId')";
+			if(isset($vehicleId)){
+				$sql = "INSERT INTO $tableName (datetime,longitude,latitude,bmap_longitude,bmap_latitude,height,speed,status,offset_distance,vehicle_id) VALUES ('$time','$longitude','$latitude','$blong','$blat','$heigh','$speed','$status','$wanderR', '$vehicleId')";
+			}else{
+				$sql = "INSERT INTO $tableName (datetime,longitude,latitude,bmap_longitude,bmap_latitude,height,speed,status,offset_distance) VALUES ('$time','$longitude','$latitude','$blong','$blat','$heigh','$speed','$status','$wanderR')";
+			}
+			
 			if(!mysql_query($sql,$con))
 			{
-				$msg = "插入表".$tableName."失败：".mysql_error();
+				$msg = "insert into ".$tableName." fails：".mysql_error();
 				$this->writeLog($msg);
 			}
 		}
 	}
 
-	function SelectSocketServer($port) 
-	{
-		global $errno, $errstr;
-		if ($port < 1024) {
-			die("Port must be a number which bigger than 1024\n");
-		}
-		
-		$socket = socket_create_listen($port);
-		if (!$socket) die("Listen $port failed");
-		
-		socket_set_nonblock($socket); // non_blocking
-		
-		while (true) 
-		{
-			$readfds = array_merge(self::$connections, array($socket));
-			$writefds = array();
-			
-			//select one link and get the pip 
-			if (socket_select($readfds, $writefds, $e = null, $t = self::$timeout)) 
-			{
-				if (in_array($socket, $readfds)) {
-					$newconn = socket_accept($socket);
-					$i = (int) $newconn;
-					$reject = '';
-					if (count(self::$connections) >= self::$maxconns) {
-						$reject = "Server full, Try again later.\n";
-					}
-
-					self::$connections[$i] = $newconn;
-
-					$writefds[$i] = $newconn;
-
-					if ($reject) {
-						socket_write($writefds[$i], $reject);
-						unset($writefds[$i]);
-						self::close($i);
-					} else {
-						echo "Client $i come.\n";
-					}
-					// remove the listening socket from the clients-with-data array
-					$key = array_search($socket, $readfds);
-					unset($readfds[$key]);
-				}
-				
-				
-				foreach ($readfds as $rfd) {
-				
-					$i = (int) $rfd;
-				
-					$line = @socket_read($rfd, 2048, PHP_NORMAL_READ);
-					if ($line === false) {
-					    
-						echo "Connection closed on socket $i.\n";
-						self::close($i);
-						continue;
-					}
-					$tmp = substr($line, -1);
-					if ($tmp != "\r" && $tmp != "\n") {
-	
-						continue;
-					}
-
-					$line = trim($line);
-					if ($line == "quit") {
-						echo "Client $i quit.\n";
-						self::close($i);
-						break;
-					}
-					if ($line) {
-						echo "Client $i >>" . $line . "\n";
-						self::InsertDB($line);
-						//$this->bar();
-					}
-				}
-				
-				//foreach ($writefds as $wfd) {
-				//	$i = (int) $wfd;
-				//	$w = socket_write($wfd, "Welcome Client $i!\n");
-				//}
-			}
-		}
-	}
-	
-	function close ($i) 
-	{
-		socket_shutdown(self::$connections[$i]);
-		socket_close(self::$connections[$i]);
-		unset(self::$connections[$i]);
-	}
 }
-new SelectSocketServer(10008);
-?>
