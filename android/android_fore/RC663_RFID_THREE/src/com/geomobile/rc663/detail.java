e5b772ffda95b4c4a78d81aa86ce8f9542df3595
Yao Lixin <yaolixinylx@gmail.com>
2014-03-02 00:10:27 +0800

    check add feature

---
+++ android/android_fore/RC663_RFID_THREE/src/com/geomobile/rc663/detail.java
@@ -0,0 +1,25 @@
+package com.geomobile.rc663;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class detail extends ScanActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         //Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("detail");
+        //this.alertMessage("message");
+      //   Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        textView.setText(message);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+}
