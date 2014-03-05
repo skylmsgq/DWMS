7e2436dd811b2914b7ba5defde1b3926f93b46f9
Bin Zhang <zhangbinsjtu@qq.com>
2014-03-05 14:27:58 +0800

    change the logo and database

--- www/App/Conf/config.php
+++ www/App/Conf/config.php
@@ -18,9 +18,10 @@ return array(
 */
 	// 添加数据库配置信息
 	'DB_TYPE'   => 'mysql',   // 数据库类型
-	'DB_HOST'   => '202.120.58.100', // 服务器地址
+	// 'DB_HOST'   => '202.120.58.100', // 远端服务器地址
+	'DB_HOST'	=> 'localhost',	// 本地服务器地址
 	'DB_USER'   => 'root',    // 用户名
-	'DB_PWD'    => 'omnilab',  // 密码
+	'DB_PWD'    => 'root1234',  // 密码
 	'DB_PORT'   => 3306,    // 端口
 	'DB_NAME'   => 'dwms',  // 数据库名
 	'DB_PREFIX' => '',  // 数据库表前缀
