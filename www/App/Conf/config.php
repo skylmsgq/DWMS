971a3250d3a9e37084a53d8d72dd1c93c060f1a9
Bin Zhang <zhangbinsjtu@qq.com>
2014-03-05 14:33:16 +0800

    sorry, I forget to recover the database password

--- www/App/Conf/config.php
+++ www/App/Conf/config.php
@@ -21,7 +21,8 @@ return array(
 	// 'DB_HOST'   => '202.120.58.100', // 远端服务器地址
 	'DB_HOST'	=> 'localhost',	// 本地服务器地址
 	'DB_USER'   => 'root',    // 用户名
-	'DB_PWD'    => 'root1234',  // 密码
+	'DB_PWD'    => 'omnilab',  // 本地密码
+	// 'DB_PWD'    => 'root1234',  // 本地密码
 	'DB_PORT'   => 3306,    // 端口
 	'DB_NAME'   => 'dwms',  // 数据库名
 	'DB_PREFIX' => '',  // 数据库表前缀
