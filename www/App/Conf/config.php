4e3c4c2452990bcdc9331b31f68c882b97749806
skylmsgq <skylmsgq@gmail.com>
2014-03-07 15:41:29 +0800

    highchart down

--- www/App/Conf/config.php
+++ www/App/Conf/config.php
@@ -23,7 +23,7 @@ return array(
 	'DB_HOST'   => 'localhost', // 服务器地址
 	'DB_USER'   => 'root',    // 用户名
 
-	'DB_PWD'    => 'omnilab',  // 本地密码
+	'DB_PWD'    => 'sky363738',  // 本地密码
 	// 'DB_PWD'    => 'root1234',  // 本地密码
 
 	'DB_PORT'   => 3306,    // 端口
