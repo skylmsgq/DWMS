0976a65858ea703ffe32afcc7b261422b5667df7
skylmsgq <skylmsgq@gmail.com>
2014-03-20 20:30:39 +0800

    chart

--- www/App/Lib/Action/Home/DistrictBusinessAction.class.php
+++ www/App/Lib/Action/Home/DistrictBusinessAction.class.php
@@ -419,20 +419,29 @@ class DistrictBusinessAction extends DistrictCommonAction{
 	public function get_chart()
 	{
 		$str="";
-		$pnum=M('production_unit')->count();
-		$tnum=M('transport_unit')->count();
-		$rnum=M('reception_unit')->count();
+		$pnum=M('production_unit')->where( array( 'jurisdiction_id' => session('jurisdiction_id') ))->count();
+		$tnum=M('transport_unit')->where( array( 'jurisdiction_id' => session('jurisdiction_id') ))->count();
+		$rnum=M('reception_unit')->where( array( 'jurisdiction_id' => session('jurisdiction_id') ))->count();
 		$manifestnum=M('manifest')->count();
 		$recordnum=M('record')->count();
 		$devicenum=M('device')->count();
 		$vehiclenum=M('vehicle')->count();
-		$tong_num=M('rfid')->where("add_method=0")->sum('waste_total');
-		$dai_num=M('rfid')->where("add_method=1")->sum('waste_total');
+		
+		// $tong_num=M('rfid')->where("add_method=0")->sum('waste_total');
+		// $dai_num=M('rfid')->where("add_method=1")->sum('waste_total');
 
-		$categories=M('rfid')->join('waste_category ON rfid.waste_category_id = waste_category.waste_category_id')->where(array('add_method'=>0))->getField('waste_category_code',true);
-		$rfid = M('rfid');
-		$condition['add_method'] = array('EQ',0);
-		$join = $rfid->join('production_unit ON rfid.production_unit_id = production_unit.production_unit_id' )->join('waste_category ON rfid.waste_category_id = waste_category.waste_category_id')->where($condition)->select();
+		$tong_num=M('manifest')->where('manifest_status>0')->sum('waste_weight');
+		$dai_num=M('manifest')->where('manifest_status>0')->sum('waste_num');
+
+		// $categories=M('rfid')->join('waste_category ON rfid.waste_category_id = waste_category.waste_category_id')->group('waste_category_code')->where(array('add_method'=>0))->getField('waste_category_code',true);
+		// $rfid = M('rfid');
+		// $condition['add_method'] = array('EQ',0);
+		// $join = $rfid->join('production_unit ON rfid.production_unit_id = production_unit.production_unit_id' )->join('waste_category ON rfid.waste_category_id = waste_category.waste_category_id')->where($condition)->select();
+
+		$categories = M('manifest')->group('waste_category_code')->getField('waste_category_code',true);
+		$manifest = M('manifest');
+		$join = $manifest->join( 'production_unit ON manifest.production_unit_id = production_unit.production_unit_id' )->where('manifest_status>0')->select();
+
 
 		$dict=array();
 		$count_waste=0;
